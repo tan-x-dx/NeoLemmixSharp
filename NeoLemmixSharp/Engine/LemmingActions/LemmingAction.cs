f11bc6b1fece2a3f0d8705654eb63562444b8651
tan-x-dx <andrewhayman19@gmail.com>
2023-04-16 14:40:19 +0100

    Tweaks to method.

--- NeoLemmixSharp/Engine/LemmingActions/LemmingAction.cs
+++ NeoLemmixSharp/Engine/LemmingActions/LemmingAction.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using NeoLemmixSharp.Engine.Directions.Orientations;
 using NeoLemmixSharp.Rendering;
-using NeoLemmixSharp.Util;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace NeoLemmixSharp.Engine.LemmingActions;
 
@@ -14,52 +14,61 @@ public abstract class LemmingAction : IEquatable<LemmingAction>
 
     private static ReadOnlyDictionary<string, LemmingAction> RegisterAllLemmingActions()
     {
-        var ids = new ArrayBasedBitArray(50);
-
         var result = new Dictionary<string, LemmingAction>();
 
-        RegisterLemmingAction(ids, result, AscenderAction.Instance);
-        RegisterLemmingAction(ids, result, BasherAction.Instance);
-        RegisterLemmingAction(ids, result, BlockerAction.Instance);
-        RegisterLemmingAction(ids, result, BuilderAction.Instance);
-        RegisterLemmingAction(ids, result, ClimberAction.Instance);
-        RegisterLemmingAction(ids, result, DehoisterAction.Instance);
-        RegisterLemmingAction(ids, result, DiggerAction.Instance);
-        RegisterLemmingAction(ids, result, DisarmerAction.Instance);
-        RegisterLemmingAction(ids, result, DrownerAction.Instance);
-        RegisterLemmingAction(ids, result, ExiterAction.Instance);
-        RegisterLemmingAction(ids, result, ExploderAction.Instance);
-        RegisterLemmingAction(ids, result, FencerAction.Instance);
-        RegisterLemmingAction(ids, result, FallerAction.Instance);
-        RegisterLemmingAction(ids, result, FloaterAction.Instance);
-        RegisterLemmingAction(ids, result, GliderAction.Instance);
-        RegisterLemmingAction(ids, result, HoisterAction.Instance);
-        RegisterLemmingAction(ids, result, JumperAction.Instance);
-        RegisterLemmingAction(ids, result, LasererAction.Instance);
-        RegisterLemmingAction(ids, result, MinerAction.Instance);
-        RegisterLemmingAction(ids, result, OhNoerAction.Instance);
-        RegisterLemmingAction(ids, result, PlatformerAction.Instance);
-        RegisterLemmingAction(ids, result, ReacherAction.Instance);
-        RegisterLemmingAction(ids, result, ShimmierAction.Instance);
-        RegisterLemmingAction(ids, result, ShruggerAction.Instance);
-        RegisterLemmingAction(ids, result, SliderAction.Instance);
-        RegisterLemmingAction(ids, result, SplatterAction.Instance);
-        RegisterLemmingAction(ids, result, StackerAction.Instance);
-        RegisterLemmingAction(ids, result, StonerAction.Instance);
-        RegisterLemmingAction(ids, result, SwimmerAction.Instance);
-        RegisterLemmingAction(ids, result, VaporiserAction.Instance);
-        RegisterLemmingAction(ids, result, WalkerAction.Instance);
+        RegisterLemmingAction(AscenderAction.Instance);
+        RegisterLemmingAction(BasherAction.Instance);
+        RegisterLemmingAction(BlockerAction.Instance);
+        RegisterLemmingAction(BuilderAction.Instance);
+        RegisterLemmingAction(ClimberAction.Instance);
+        RegisterLemmingAction(DehoisterAction.Instance);
+        RegisterLemmingAction(DiggerAction.Instance);
+        RegisterLemmingAction(DisarmerAction.Instance);
+        RegisterLemmingAction(DrownerAction.Instance);
+        RegisterLemmingAction(ExiterAction.Instance);
+        RegisterLemmingAction(ExploderAction.Instance);
+        RegisterLemmingAction(FencerAction.Instance);
+        RegisterLemmingAction(FallerAction.Instance);
+        RegisterLemmingAction(FloaterAction.Instance);
+        RegisterLemmingAction(GliderAction.Instance);
+        RegisterLemmingAction(HoisterAction.Instance);
+        RegisterLemmingAction(JumperAction.Instance);
+        RegisterLemmingAction(LasererAction.Instance);
+        RegisterLemmingAction(MinerAction.Instance);
+        RegisterLemmingAction(OhNoerAction.Instance);
+        RegisterLemmingAction(PlatformerAction.Instance);
+        RegisterLemmingAction(ReacherAction.Instance);
+        RegisterLemmingAction(ShimmierAction.Instance);
+        RegisterLemmingAction(ShruggerAction.Instance);
+        RegisterLemmingAction(SliderAction.Instance);
+        RegisterLemmingAction(SplatterAction.Instance);
+        RegisterLemmingAction(StackerAction.Instance);
+        RegisterLemmingAction(StonerAction.Instance);
+        RegisterLemmingAction(SwimmerAction.Instance);
+        RegisterLemmingAction(VaporiserAction.Instance);
+        RegisterLemmingAction(WalkerAction.Instance);
+
+        var numberOfUniqueIds = result
+            .Values
+            .Select(la => la.ActionId)
+            .Distinct()
+            .Count();
+
+        if (numberOfUniqueIds != result.Count)
+        {
+            var ids = string.Join(',', result
+                .Values
+                .Select(la => la.ActionId)
+                .OrderBy(i => i));
+
+            throw new Exception($"Duplicated ID: {ids}");
+        }
 
         return new ReadOnlyDictionary<string, LemmingAction>(result);
-    }
 
-    private static void RegisterLemmingAction(IBitArray ids, IDictionary<string, LemmingAction> dict, LemmingAction lemmingAction)
-    {
-        dict.Add(lemmingAction.LemmingActionName, lemmingAction);
-
-        if (!ids.SetBit(lemmingAction.ActionId))
+        void RegisterLemmingAction(LemmingAction lemmingAction)
         {
-            throw new Exception($"Duplicated ID: {lemmingAction.LemmingActionName}");
+            result.Add(lemmingAction.LemmingActionName, lemmingAction);
         }
     }
 
