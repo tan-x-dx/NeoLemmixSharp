e366baf1405de520597bb77af718dcbf3dab30ea
tan-x-dx <andrewhayman19@gmail.com>
2023-04-18 20:20:37 +0100

    Basic font rendering test.

--- NeoLemmixSharp/NeoLemmixGame.cs
+++ NeoLemmixSharp/NeoLemmixGame.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using NeoLemmixSharp.LevelBuilding;
+using NeoLemmixSharp.Rendering.Text;
 using NeoLemmixSharp.Screen;
 using NeoLemmixSharp.Util;
 using System;
@@ -16,6 +17,7 @@ public sealed class NeoLemmixGame : Game, IGameWindow
 
     private Point _gameResolution = new(960, 720);
     private SpriteBatch _spriteBatch;
+    private MenuFont _menuFont;
 
     public int WindowWidth => _graphics.PreferredBackBufferWidth;
     public int WindowHeight => _graphics.PreferredBackBufferHeight;
@@ -69,6 +71,8 @@ public sealed class NeoLemmixGame : Game, IGameWindow
 
     protected override void LoadContent()
     {
+        _menuFont = new MenuFont(Content);
+
         _spriteBatch = new SpriteBatch(GraphicsDevice);
         _graphics.PreferredBackBufferWidth = _gameResolution.X;
         _graphics.PreferredBackBufferHeight = _gameResolution.Y;
@@ -85,7 +89,7 @@ public sealed class NeoLemmixGame : Game, IGameWindow
         //   "C:\\Users\\andre\\Documents\\NeoLemmix_v12.12.5\\levels\\Amiga Lemmings\\Oh No! More Lemmings\\Tame\\05_Snuggle_up_to_a_Lemming.nxlv";
         //  "C:\\Users\\andre\\Documents\\NeoLemmix_v12.12.5\\levels\\Amiga Lemmings\\Lemmings\\Tricky\\05_Careless_clicking_costs_lives.nxlv";
         //   "C:\\Users\\andre\\Documents\\NeoLemmix_v12.12.5\\levels\\LemRunner\\Industry\\TheNightShift.nxlv";
-         //  "C:\\Users\\andre\\Documents\\NeoLemmix_v12.12.5\\levels\\Amiga Lemmings\\Lemmings\\Tricky\\04_Here's_one_I_prepared_earlier.nxlv";
+        //  "C:\\Users\\andre\\Documents\\NeoLemmix_v12.12.5\\levels\\Amiga Lemmings\\Lemmings\\Tricky\\04_Here's_one_I_prepared_earlier.nxlv";
         //"C:\\Users\\andre\\Documents\\NeoLemmix_v12.12.5\\levels\\IntegralLemmingsV5\\Alpha\\TheseLemmingsAndThoseLemmings.nxlv";
         //"C:\\Users\\andre\\Documents\\NeoLemmix_v12.12.5\\levels\\CuttingItClose.nxlv";
         //    "C:\\Users\\andre\\Documents\\NeoLemmix_v12.12.5\\levels\\scrollTest.nxlv";
@@ -120,6 +124,8 @@ public sealed class NeoLemmixGame : Game, IGameWindow
         _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
         Screen.Render(_spriteBatch);
+
+        _menuFont.RenderText(_spriteBatch, "Test lol :)", 30, 20);
 
         _spriteBatch.End();
 
---
+++ NeoLemmixSharp/Rendering/Text/MenuFont.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeoLemmixSharp.Rendering.Text;
+
+public sealed class MenuFont : INeoLemmixFont
+{
+    private readonly Texture2D _texture;
+    private readonly char[] _chars = Enumerable.Range(32, 95).Select(i => (char)i).ToArray();
+
+    public MenuFont(ContentManager content)
+    {
+        _texture = content.Load<Texture2D>("Fonts/menu_font");
+    }
+
+    public void Dispose()
+    {
+        _texture.Dispose();
+    }
+
+    public void RenderText(
+        SpriteBatch spriteBatch,
+        IEnumerable<char> charactersToRender,
+        int x,
+        int y)
+    {
+        var dest = new Rectangle(x, y, 16, 19);
+        foreach (var c in charactersToRender.Where(k => k > 31 && k < 127))
+        {
+            var source = new Rectangle(16 * (c - 33), 0, 16, 19);
+            spriteBatch.Draw(_texture, dest, source, Color.White);
+            dest.X += 16;
+        }
+    }
+}
