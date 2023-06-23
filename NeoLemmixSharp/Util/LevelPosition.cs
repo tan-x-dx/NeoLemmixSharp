497509b6c46d5a56629f7e64f39fe21fbc150f68
tan-x-dx <andrewhayman19@gmail.com>
2023-06-23 19:20:26 +0100

    Added equality comparer class for LevelPosition.

--- NeoLemmixSharp/Util/LevelPosition.cs
+++ NeoLemmixSharp/Util/LevelPosition.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NeoLemmixSharp.Util;
 
@@ -34,4 +35,24 @@ public readonly struct LevelPosition : IEquatable<LevelPosition>
                                          1088251;
 
     public override string ToString() => $"[{X},{Y}]";
+}
+
+public sealed class LevelPositionEqualityComparer : IEqualityComparer<LevelPosition>, IEquatable<LevelPositionEqualityComparer>
+{
+    public static LevelPositionEqualityComparer Instance { get; } = new();
+
+    private LevelPositionEqualityComparer()
+    {
+    }
+
+    public bool Equals(LevelPosition left, LevelPosition right) => left.X == right.X &&
+                                                                   left.Y == right.Y;
+
+    public int GetHashCode(LevelPosition levelPosition) => 3790121 * levelPosition.X +
+                                                           2885497 * levelPosition.Y +
+                                                           1088251;
+
+    bool IEquatable<LevelPositionEqualityComparer>.Equals(LevelPositionEqualityComparer? other) => ReferenceEquals(other, Instance);
+    public override bool Equals(object? obj) => ReferenceEquals(obj, Instance);
+    public override int GetHashCode() => nameof(LevelPositionEqualityComparer).GetHashCode();
 }
