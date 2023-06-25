c70fed5e73a2410fd1d7b57b273058b7e52e2806
tan-x-dx <andrewhayman19@gmail.com>
2023-06-25 13:43:34 +0100

    Undo changes - doesn't work.

--- NeoLemmixSharp/Engine/Lemming.cs
+++ NeoLemmixSharp/Engine/Lemming.cs
@@ -28,12 +28,12 @@ public sealed class Lemming
     public bool PlacedBrick;
     public bool StackLow;
     public bool InitialFall;
-
+    public bool EndOfAnimation;
     public bool LaserHit;
 
     public bool Debug;
 
-    public int AnimationFrame;
+    public int AnimationFrame { get; set; }
     public int AscenderProgress;
     public int NumberOfBricksLeft;
     public int DisarmingFrames;
@@ -58,7 +58,6 @@ public sealed class Lemming
     public LemmingRenderer Renderer { get; }
 
     public bool ShouldTick => true;
-    public bool EndOfAnimation => AnimationFrame == CurrentAction.NumberOfAnimationFrames && CurrentAction.IsOneTimeAction;
 
     public Lemming(
         Orientation? orientation = null,
@@ -111,6 +110,11 @@ public sealed class Lemming
             else
             {
                 AnimationFrame = 0;
+            }
+
+            if (CurrentAction.IsOneTimeAction)
+            {
+                EndOfAnimation = true;
             }
         }
 
