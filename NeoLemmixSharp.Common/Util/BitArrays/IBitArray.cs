e39a332f3072b9819c7f5d0eb3bf6a2bc70415d6
tan-x-dx <andrewhayman19@gmail.com>
2023-07-14 15:44:37 +0100

    Tweaks to methods.

--- NeoLemmixSharp.Common/Util/BitArrays/IBitArray.cs
+++ NeoLemmixSharp.Common/Util/BitArrays/IBitArray.cs
@@ -7,14 +7,15 @@ public interface IBitArray : ICollection<int>, IReadOnlyCollection<int>, IClonea
 {
     int Length { get; }
     bool AnyBitsSet { get; }
+    new int Count { get; }
 
     bool GetBit(int index);
     bool SetBit(int index);
     bool ClearBit(int index);
 
-    void ICollection<int>.Add(int i) => SetBit(i);
-    bool ICollection<int>.Contains(int i) => GetBit(i);
-    bool ICollection<int>.Remove(int i) => ClearBit(i);
+    void ICollection<int>.Add(int i) => throw new NotSupportedException("Cannot add to a fixed-size collection");
+    bool ICollection<int>.Contains(int i) => i >= 0 && i < Length && GetBit(i);
+    bool ICollection<int>.Remove(int i) => i >= 0 && i < Length && ClearBit(i);
 
     [DebuggerBrowsable(DebuggerBrowsableState.Never)]
     bool ICollection<int>.IsReadOnly => false;
