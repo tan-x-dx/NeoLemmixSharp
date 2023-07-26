64f0f21debe797a0081593ba2c983c2a321109fa
tan-x-dx <andrewhayman19@gmail.com>
2023-07-26 17:41:54 +0100

    Added timers and some initial implementations.

---
+++ NeoLemmixSharp.Engine/Engine/Timer/CountDownTimer.cs
@@ -0,0 +1,16 @@
+﻿namespace NeoLemmixSharp.Engine.Engine.Timer;
+
+public sealed class CountDownTimer : ITimer
+{
+    public int ElapsedTicks { get; private set; }
+
+    public void Tick()
+    {
+
+    }
+
+    public ReadOnlySpan<int> AsSpan()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ NeoLemmixSharp.Engine/Engine/Timer/ITimer.cs
@@ -0,0 +1,9 @@
+﻿namespace NeoLemmixSharp.Engine.Engine.Timer;
+
+public interface ITimer
+{
+    int ElapsedTicks { get; }
+
+    void Tick();
+    ReadOnlySpan<int> AsSpan();
+}
