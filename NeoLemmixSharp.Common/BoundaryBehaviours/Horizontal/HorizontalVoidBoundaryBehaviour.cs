1ae53c104997896fe3b6fab67e51c7c58a3c07a2
tan-x-dx <andrewhayman19@gmail.com>
2023-09-16 20:59:48 +0100

    Corrected methods.

--- NeoLemmixSharp.Common/BoundaryBehaviours/Horizontal/HorizontalVoidBoundaryBehaviour.cs
+++ NeoLemmixSharp.Common/BoundaryBehaviours/Horizontal/HorizontalVoidBoundaryBehaviour.cs
@@ -14,5 +14,5 @@ public sealed class HorizontalVoidBoundaryBehaviour : IHorizontalBoundaryBehavio
     [Pure]
     public int NormaliseX(int x) => x;
     [Pure]
-    public int GetHorizontalDelta(int x1, int x2) => Math.Abs(x1 - x2);
+    public int GetHorizontalDelta(int x1, int x2) => x2 - x1;
 }
