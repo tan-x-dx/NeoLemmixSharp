ca72cd4dd1265948ac11634316e4538a039a80f2
tan-x-dx <andrewhayman19@gmail.com>
2023-09-24 15:40:50 +0100

    Added HatchGadget and associated type.

---
+++ NeoLemmixSharp.Engine/Level/Gadgets/Functional/HatchGadget.cs
@@ -0,0 +1,35 @@
+﻿using NeoLemmixSharp.Common.Util;
+using NeoLemmixSharp.Common.Util.LevelRegion;
+using NeoLemmixSharp.Engine.Level.Gadgets.GadgetTypes;
+using NeoLemmixSharp.Engine.Level.Gadgets.Interactions;
+using NeoLemmixSharp.Engine.Level.Orientations;
+
+namespace NeoLemmixSharp.Engine.Level.Gadgets.Functional;
+
+public sealed class HatchGadget : GadgetBase
+{
+    public override GadgetType Type => HatchGadgetType.Instance;
+    public override Orientation Orientation { get; }
+    public LevelPosition SpawnPosition { get; }
+
+    public HatchGadget(
+        int id,
+        RectangularLevelRegion gadgetBounds,
+        Orientation orientation,
+        LevelPosition spawnPosition)
+        : base(id, gadgetBounds)
+    {
+        Orientation = orientation;
+        SpawnPosition = spawnPosition;
+    }
+
+    public override void Tick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IGadgetInput? GetInputWithName(string inputName)
+    {
+        throw new NotImplementedException();
+    }
+}
--- NeoLemmixSharp.Engine/Level/Gadgets/GadgetTypes/GadgetType.cs
+++ NeoLemmixSharp.Engine/Level/Gadgets/GadgetTypes/GadgetType.cs
@@ -25,6 +25,7 @@ public abstract class GadgetType : IExtendedEnumType<GadgetType>
             SawBladeGadgetType.Instance,
             FunctionalGadgetType.Instance,
             LogicGateGadgetType.Instance,
+            HatchGadgetType.Instance
         };
 
         result.ValidateUniqueIds();
