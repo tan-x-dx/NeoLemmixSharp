23a34d7f52eb9b2878d2bfc7bb589ef65dd4affb
tan-x-dx <andrewhayman19@gmail.com>
2023-10-30 10:24:18 +0000

    Added new project for menus.

---
+++ NeoLemmixSharp.Menu/MenuScreen.cs
@@ -0,0 +1,33 @@
+﻿using NeoLemmixSharp.Common.Rendering;
+using NeoLemmixSharp.Common.Screen;
+using NeoLemmixSharp.Common.Util;
+using NeoLemmixSharp.Menu.Rendering;
+
+namespace NeoLemmixSharp.Menu;
+
+public sealed class MenuScreen : IBaseScreen
+{
+    public MenuScreenRenderer MenuScreenRenderer { get; }
+
+    public IGameWindow GameWindow { get; set; }
+    IScreenRenderer IBaseScreen.ScreenRenderer => MenuScreenRenderer;
+    public string ScreenTitle => "NeoLemmixSharp";
+    public bool IsDisposed { get; private set; }
+
+    public MenuScreen()
+    {
+        
+    }
+
+    public void Tick()
+    {
+    }
+
+    public void OnWindowSizeChanged()
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+}
---
+++ NeoLemmixSharp.Menu/Rendering/MenuScreenRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using NeoLemmixSharp.Common.Rendering;
+using NeoLemmixSharp.Common.Util;
+
+namespace NeoLemmixSharp.Menu.Rendering;
+
+public sealed class MenuScreenRenderer : IScreenRenderer
+{
+    public bool IsDisposed { get; private set; }
+    public IGameWindow GameWindow { get; set; }
+
+    public void RenderScreen(SpriteBatch spriteBatch)
+    {
+    }
+
+    public void OnWindowSizeChanged(int windowWidth, int windowHeight)
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+}
