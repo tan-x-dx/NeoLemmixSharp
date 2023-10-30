9c4a86ff0366aaef5e2e9b5aba861c66ce11972e
tan-x-dx <andrewhayman19@gmail.com>
2023-10-30 13:39:19 +0000

    Added method info.

--- NeoLemmixSharp.Common/Util/HelperMethods.cs
+++ NeoLemmixSharp.Common/Util/HelperMethods.cs
@@ -2,6 +2,11 @@
 
 public static class HelperMethods
 {
+    /// <summary>
+    /// Disposes of the object and then sets the reference to null
+    /// </summary>
+    /// <typeparam name="T">The type of the object to dispose</typeparam>
+    /// <param name="obj">The object to dispose</param>
     public static void DisposeOf<T>(ref T obj)
         where T : class, IDisposable
     {
