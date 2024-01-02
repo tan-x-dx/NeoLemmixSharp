c655b4f110db943c7d00e8888df0cb955c101aa2
tan-x-dx <andrewhayman19@gmail.com>
2024-01-02 17:46:38 +0000

    Tweaks to methods.

--- NeoLemmixSharp.Common/Util/DihedralTransformation.cs
+++ NeoLemmixSharp.Common/Util/DihedralTransformation.cs
@@ -5,118 +5,111 @@ namespace NeoLemmixSharp.Common.Util;
 
 public readonly struct DihedralTransformation
 {
-    public static DihedralTransformation GetForTransformation(
-        bool flipHorizontally,
-        bool flipVertically,
-        bool rotate)
-    {
-        var rotNum = rotate
-            ? 1
-            : 0;
+	public static DihedralTransformation GetForTransformation(
+		bool flipHorizontally,
+		bool flipVertically,
+		bool rotate)
+	{
+		var rotNum = rotate
+			? 1
+			: 0;
 
-        if (flipVertically)
-        {
-            flipHorizontally = !flipHorizontally;
-            rotNum += 2;
-        }
+		if (flipVertically)
+		{
+			flipHorizontally = !flipHorizontally;
+			rotNum += 2;
+		}
 
-        return new DihedralTransformation(rotNum, flipHorizontally);
-    }
+		return new DihedralTransformation(rotNum, flipHorizontally);
+	}
 
-    public static DihedralTransformation GetForTransformation(
-        bool flipHorizontally,
-        int rotNum)
-    {
-        return new DihedralTransformation(rotNum, flipHorizontally);
-    }
+	private readonly int _r;
+	private readonly int _a;
+	private readonly int _b;
 
-    private readonly int _r;
-    private readonly int _a;
-    private readonly int _b;
+	private readonly int _f;
+	private readonly int _m;
 
-    private readonly int _f;
-    private readonly int _m;
+	public DihedralTransformation()
+		: this(0, false)
+	{
+	}
 
-    public DihedralTransformation()
-        : this(0, false)
-    {
-    }
+	public DihedralTransformation(int r, bool flip)
+	{
+		_r = r & 3;
 
-    private DihedralTransformation(int r, bool flip)
-    {
-        _r = r & 3;
+		switch (_r)
+		{
+			case 0:
+				_a = 1;
+				_b = 0;
+				break;
+			case 1:
+				_a = 0;
+				_b = 1;
+				break;
+			case 2:
+				_a = -1;
+				_b = 0;
+				break;
+			case 3:
+				_a = 0;
+				_b = -1;
+				break;
+		}
 
-        switch (_r)
-        {
-            case 0:
-                _a = 1;
-                _b = 0;
-                break;
-            case 1:
-                _a = 0;
-                _b = 1;
-                break;
-            case 2:
-                _a = -1;
-                _b = 0;
-                break;
-            case 3:
-                _a = 0;
-                _b = -1;
-                break;
-        }
+		if (flip)
+		{
+			_f = 1;
+			_m = -1;
+		}
+		else
+		{
+			_f = 0;
+			_m = 1;
+		}
+	}
 
-        if (flip)
-        {
-            _f = 1;
-            _m = -1;
-        }
-        else
-        {
-            _f = 0;
-            _m = 1;
-        }
-    }
+	public override string ToString() => $"Rot {_r}|{FlipString}";
 
-    public override string ToString() => $"Rot {_r}|{FlipString}";
+	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+	private string FlipString => _f == 0 ? string.Empty : "Flip";
 
-    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-    private string FlipString => _f == 0 ? string.Empty : "Flip";
+	public void Transform(
+		int x,
+		int y,
+		int width,
+		int height,
+		out int x0,
+		out int y0)
+	{
+		var w = W(width, height);
+		var h = H(width, height);
+		var s = _f * Choose(width, height);
 
-    public void Transform(
-        int x,
-        int y,
-        int width,
-        int height,
-        out int x0,
-        out int y0)
-    {
-        var w = W(width, height);
-        var h = H(width, height);
-        var s = _f * Choose(width, height);
+		x0 = s + _m * (_a * x - _b * y + w);
+		y0 = _b * x + _a * y + h;
+	}
 
-        x0 = s + _m * (_a * x - _b * y + w);
-        y0 = _b * x + _a * y + h;
-    }
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private int W(int w, int h) => _r switch
+	{
+		1 => h,
+		2 => w,
+		_ => 0
+	};
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private int W(int w, int h) => _r switch
-    {
-        1 => h,
-        2 => w,
-        _ => 0
-    };
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private int H(int w, int h) => _r switch
+	{
+		2 => h,
+		3 => w,
+		_ => 0
+	};
 
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private int H(int w, int h) => _r switch
-    {
-        2 => h,
-        3 => w,
-        _ => 0
-    };
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private int Choose(int w, int h) => (_r & 1) == 1
-        ? h
-        : w;
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private int Choose(int w, int h) => (_r & 1) == 1
+		? h
+		: w;
 }
