0518a1deb80a9f60f4a2eb3eefdf847bf95bdc47
tan-x-dx <andrewhayman19@gmail.com>
2024-02-18 13:31:36 +0000

    Initial implementation of texture stitching.

--- NeoLemmixSharp.Engine/LevelBuilding/LevelReading/NeoLemmixCompat/Readers/GadgetReaders/GadgetTranslation/GadgetTranslator.TextureStitching.cs
+++ NeoLemmixSharp.Engine/LevelBuilding/LevelReading/NeoLemmixCompat/Readers/GadgetReaders/GadgetTranslation/GadgetTranslator.TextureStitching.cs
@@ -23,16 +23,37 @@ public sealed partial class GadgetTranslator
         if (animationDataSpan.IsEmpty)
             return primaryTexture;
 
-        var maxSpriteWidth = primaryTexture.Width;
-        var maxSpriteHeight = primaryTexture.Height;
+        var maxTextureWidth = primaryTexture.Width;
+        var maxTextureHeight = primaryTexture.Height;
 
         foreach (var animationData in animationDataSpan)
         {
-            maxSpriteWidth += animationData.Texture!.Width;
-            maxSpriteHeight = Math.Max(maxSpriteHeight, animationData.Texture.Height);
+            var texture = animationData.Texture!;
+
+            maxTextureWidth += texture.Width;
+            maxTextureHeight = Math.Max(maxTextureHeight, texture.Height);
         }
 
-        var result = new Texture2D(_graphicsDevice, maxSpriteWidth, maxSpriteHeight);
+        var result = new Texture2D(_graphicsDevice, maxTextureWidth * (1 + animationDataSpan.Length), maxTextureHeight);
+        var resultTextureData = new uint[result.Width * result.Height];
+
+        var primaryTextureData = new uint[primaryTexture.Width * primaryTexture.Height];
+        primaryTexture.GetData(primaryTextureData);
+
+        var xOffset = StitchTexture(resultTextureData, result.Width, result.Height, 0, primaryTexture);
+
+        foreach (var animationData in animationDataSpan)
+        {
+            var animationTexture = animationData.Texture!;
+            var xOffsetDelta = StitchTexture(resultTextureData, result.Width, result.Height, xOffset, animationTexture);
+
+            xOffset += xOffsetDelta;
+        }
+
+        result.SetData(resultTextureData);
+
+        primaryTexture.Dispose();
+        DisposeOfSourceTextures(archetypeData);
 
         return result;
     }
@@ -76,7 +97,7 @@ public sealed partial class GadgetTranslator
         gadgetName.AsSpan().CopyTo(fileNameSpan);
         fileNameSpan[gadgetName.Length] = '_';
 
-        foreach (var animationData in archetypeData.AnimationData)
+        foreach (var animationData in animationDataSpan)
         {
             var i = gadgetName.Length + 1;
             animationData.Name.AsSpan().CopyTo(fileNameSpan[i..]);
@@ -97,5 +118,39 @@ public sealed partial class GadgetTranslator
         }
 
         return primaryTexture;
+    }
+
+    private static int StitchTexture(
+        uint[] resultTextureData,
+        int resultTextureWidth,
+        int resultTextureHeight,
+        int xOffset,
+        Texture2D sourceTexture)
+    {
+        var animationTextureData = new uint[sourceTexture.Width * sourceTexture.Height];
+        sourceTexture.GetData(animationTextureData);
+
+        var animationTextureWrapper = new SpanWrapper2D(animationTextureData, sourceTexture.Width, sourceTexture.Height, 0, 0, sourceTexture.Width, sourceTexture.Height);
+        var resultTextureWrapper = new SpanWrapper2D(resultTextureData, resultTextureWidth, resultTextureHeight, xOffset, 0, sourceTexture.Width, sourceTexture.Height);
+
+        for (var x = 0; x < animationTextureWrapper.Width; x++)
+        {
+            for (var y = 0; y < animationTextureWrapper.Height; y++)
+            {
+                resultTextureWrapper[x, y] = animationTextureWrapper[x, y];
+            }
+        }
+
+        return sourceTexture.Width;
+    }
+
+    private static void DisposeOfSourceTextures(NeoLemmixGadgetArchetypeData archetypeData)
+    {
+        var animationDataSpan = CollectionsMarshal.AsSpan(archetypeData.AnimationData);
+        foreach (var animationData in animationDataSpan)
+        {
+            animationData.Texture!.Dispose();
+            animationData.Texture = null;
+        }
     }
 }
