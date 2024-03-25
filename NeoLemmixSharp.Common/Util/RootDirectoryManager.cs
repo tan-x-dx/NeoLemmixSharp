788834545c7babf1ec444bb9ca39f83b96059426
tan-x-dx <andrewhayman19@gmail.com>
2024-03-25 19:48:29 +0000

    Added helper comment for new devs.

--- NeoLemmixSharp.Common/Util/RootDirectoryManager.cs
+++ NeoLemmixSharp.Common/Util/RootDirectoryManager.cs
@@ -9,8 +9,16 @@ public static class RootDirectoryManager
 
     public static string RootDirectory { get; private set; } = null!;
 
+#if DEBUG
     private const string ConfigFileName = "DebugConfig.txt";
+#endif
 
+    /*
+     * If this is your first time building in a dev environment, the program has probably thrown an error in this file!
+     *
+     * You need to create a file called DebugConfig.txt in the NeoLemmixSharp project directory.
+     * Inside that file, you need to copy/paste the filepath to your NeoLemmix directory.
+     */
     private static string ReadRootDirectoryForConfigFile()
     {
 #if DEBUG
