1e6a6a5c6f3170f287e829fce322741189377536
tan-x-dx <andrewhayman19@gmail.com>
2024-06-30 13:00:56 +0100

    Initial implementations of level writing.

---
+++ NeoLemmixSharp.Engine/LevelBuilding/LevelWriting/LevelComponentWriting/StringComponentWriter.cs
@@ -0,0 +1,76 @@
+﻿using NeoLemmixSharp.Engine.LevelBuilding.Data;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace NeoLemmixSharp.Engine.LevelBuilding.LevelWriting.LevelComponentWriting;
+
+public static class StringComponentWriter
+{
+    private const int StringBufferSize = 1024;
+
+    [SkipLocalsInit]
+    public static void WriteStringSection(BinaryWriter writer, LevelData levelData)
+    {
+        WriteSectionIdentifier(writer);
+
+        var stringIdLookup = GenerateStringIdLookup(levelData);
+
+        writer.Write((ushort)stringIdLookup.Count);
+
+        Span<byte> buffer = new byte[StringBufferSize];
+
+        var utf8Encoding = Encoding.UTF8;
+
+        foreach (var (stringToWrite, id) in stringIdLookup.OrderBy(kvp => kvp.Value))
+        {
+            writer.Write((ushort)id);
+
+            var byteCount = utf8Encoding.GetBytes(stringToWrite, buffer);
+
+            writer.Write(buffer[..byteCount]);
+        }
+    }
+
+    private static void WriteSectionIdentifier(BinaryWriter writer)
+    {
+        ReadOnlySpan<byte> sectionIdentifier = [0x26, 0x44];
+        writer.Write(sectionIdentifier);
+    }
+
+    private static Dictionary<string, int> GenerateStringIdLookup(LevelData levelData)
+    {
+        var result = new Dictionary<string, int>();
+
+        TryAdd(levelData.LevelTitle);
+        TryAdd(levelData.LevelAuthor);
+
+        foreach (var text in levelData.PreTextLines)
+        {
+            TryAdd(text);
+        }
+
+        foreach (var text in levelData.PostTextLines)
+        {
+            TryAdd(text);
+        }
+
+        foreach (var terrainArchetypeData in levelData.TerrainArchetypeData)
+        {
+            TryAdd(terrainArchetypeData.Style!);
+            TryAdd(terrainArchetypeData.TerrainPiece!);
+        }
+
+        foreach (var gadgetData in levelData.AllGadgetData)
+        {
+            TryAdd(gadgetData.Style);
+            TryAdd(gadgetData.GadgetPiece);
+        }
+
+        return result;
+
+        void TryAdd(string s)
+        {
+            result.TryAdd(s, result.Count);
+        }
+    }
+}
---
+++ NeoLemmixSharp.Engine/LevelBuilding/LevelWriting/LevelWriter.cs
@@ -0,0 +1,41 @@
+﻿using NeoLemmixSharp.Engine.LevelBuilding.Data;
+using NeoLemmixSharp.Engine.LevelBuilding.LevelWriting.LevelComponentWriting;
+
+namespace NeoLemmixSharp.Engine.LevelBuilding.LevelWriting;
+
+public sealed class LevelWriter : IDisposable
+{
+    private readonly LevelData _levelData;
+    private readonly Version _version;
+
+    public LevelWriter(LevelData levelData, Version version)
+    {
+        _levelData = levelData;
+        _version = version;
+    }
+
+    public void WriteToFile(string filePath)
+    {
+        using var fileStream = new FileStream(filePath, FileMode.Create);
+        using var writer = new BinaryWriter(fileStream);
+
+        WriteVersion(writer);
+
+        StringComponentWriter.WriteStringSection(writer, _levelData);
+    }
+
+    private void WriteVersion(BinaryWriter writer)
+    {
+        writer.Write((ushort)_version.Major);
+        writer.Write('.');
+        writer.Write((ushort)_version.Minor);
+        writer.Write('.');
+        writer.Write((ushort)_version.Build);
+        writer.Write('.');
+        writer.Write((ushort)_version.Revision);
+    }
+
+    public void Dispose()
+    {
+    }
+}
