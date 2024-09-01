fe108f2efd2b82bf36610e8d2459287bf2b94c85
tan-x-dx <andrewhayman19@gmail.com>
2024-09-01 11:56:05 +0100

    Minor tweak.

--- NeoLemmixSharp.Common/Util/TextRenderingHelpers.cs
+++ NeoLemmixSharp.Common/Util/TextRenderingHelpers.cs
@@ -35,6 +35,7 @@ public static class TextRenderingHelpers
         < 10 => 1,
         < 100 => 2,
         < 1000 => 3,
-        _ => 4 // We're not going to be dealing with numbers above a few thousand
+        < 10000 => 4,
+        _ => 5 // We're not going to be dealing with numbers above a few thousand
     };
 }
