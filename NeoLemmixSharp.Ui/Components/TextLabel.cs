2b5bef252356107327ba16cfa52253d148fcd0d5
tan-x-dx <andrewhayman19@gmail.com>
2024-11-23 11:38:41 +0000

    More tweaks.

--- NeoLemmixSharp.Ui/Components/TextLabel.cs
+++ NeoLemmixSharp.Ui/Components/TextLabel.cs
@@ -1,17 +1,27 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
+using NeoLemmixSharp.Common.Rendering.Text;
 using NeoLemmixSharp.Common.Util;
-using NeoLemmixSharp.Ui.Data;
+using NeoLemmixSharp.Ui.Components.Buttons;
 
 namespace NeoLemmixSharp.Ui.Components;
 
-public sealed class TextLabel : Component
+public sealed class TextLabel : Button
 {
-    public TextLabel(int x, int y, int w, string message)
-        : base(x, y, w, UiConstants.StandardButtonHeight, message) { }
+    public TextLabel(int x, int y, string message, ColorPacket colors)
+        : base(x, y, 0, 0, message)
+    {
+        SetSize(MenuFont.GlyphWidth * message.Length, MenuFont.GlyphHeight);
+
+        Colors = colors;
+    }
 
     public override bool ContainsPoint(LevelPosition pos) => false;
 
     protected override void RenderComponent(SpriteBatch spriteBatch)
     {
+        var colors = Colors.AsSpan();
+        var color = colors[(int)State];
+
+        FontBank.MenuFont.RenderText(spriteBatch, Label, Left, Top, 1, color);
     }
 }
--- NeoLemmixSharp.Ui/Data/UiConstants.cs
+++ NeoLemmixSharp.Ui/Data/UiConstants.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.Xna.Framework;
-using NeoLemmixSharp.Ui.Components;
+﻿using NeoLemmixSharp.Ui.Components;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace NeoLemmixSharp.Ui.Data;
 
@@ -8,7 +9,7 @@ public static class UiConstants
     public const int StandardButtonHeight = 32;
 
     public const int StandardInset = 8;
-    public const int TwiceStandardInset = StandardInset << 1;
+    public const int TwiceStandardInset = StandardInset * 2;
 
     public const int RaisedRectangleBorder = 2;
 
@@ -16,9 +17,13 @@ public static class UiConstants
     internal const float FontScaleFactor = 0.3f;
     internal const float FontGlyphWidthMultiplier = FontGlyphWidth * FontScaleFactor;
 
-    public static ColorPacket RectangularButtonDefaultColours => new(
-       new Color((byte)0x44, (byte)0x44, (byte)0x44, (byte)0xff),
-       new Color((byte)0x66, (byte)0x66, (byte)0x66, (byte)0xff),
-       new Color((byte)0x88, (byte)0x88, (byte)0x88, (byte)0xff),
-       new Color((byte)0x00, (byte)0x66, (byte)0x00, (byte)0xff));
+    private static ReadOnlySpan<uint> RawDefaultColors =>
+    [
+        0xff444444,
+        0xff666666,
+        0xff888888,
+        0xff006600
+    ];
+
+    public static ColorPacket RectangularButtonDefaultColours => Unsafe.As<uint, ColorPacket>(ref MemoryMarshal.GetReference(RawDefaultColors));
 }
