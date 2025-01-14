90dee6a12e8f822e63e2a5d76da686badbbb23e3
tan-x-dx <andrewhayman19@gmail.com>
2025-01-14 15:38:36 +0000

    Refactored these gadgets.

--- NeoLemmixSharp.Engine/Level/Gadgets/FunctionalGadgets/BinaryLogic/AndGateGadget.cs
+++ NeoLemmixSharp.Engine/Level/Gadgets/FunctionalGadgets/BinaryLogic/AndGateGadget.cs
@@ -1,10 +1,13 @@
-﻿using NeoLemmixSharp.Engine.Level.Orientations;
+﻿using NeoLemmixSharp.Common.Util.Collections;
+using NeoLemmixSharp.Engine.Level.Gadgets.Interactions;
+using NeoLemmixSharp.Engine.Level.Orientations;
 
 namespace NeoLemmixSharp.Engine.Level.Gadgets.FunctionalGadgets.BinaryLogic;
 
-public sealed class AndGateGadget : LogicGateGadget
+public sealed class AndGateGadget : LogicGateGadget, IPerfectHasher<AndGateGadget.AndGateGadgetInput>
 {
-    private readonly LogicGateGadgetInput[] _inputs;
+    private readonly int _numberOfInputs;
+    private readonly SimpleSet<AndGateGadget, AndGateGadgetInput> _set;
 
     public AndGateGadget(
         int id,
@@ -15,25 +18,56 @@ public sealed class AndGateGadget : LogicGateGadget
         if (inputNames.Length < 2)
             throw new ArgumentException("Expected at least 2 inputs!");
 
-        _inputs = new LogicGateGadgetInput[inputNames.Length];
+        _numberOfInputs = inputNames.Length;
+        _set = new SimpleSet<AndGateGadget, AndGateGadgetInput>(this, false);
 
         for (var i = 0; i < inputNames.Length; i++)
         {
-            var inputName = inputNames[i];
-            var input = new LogicGateGadgetInput(inputName, this);
-            _inputs[i] = input;
+            var input = new AndGateGadgetInput(i, inputNames[i], this);
             RegisterInput(input);
         }
     }
 
     public override void EvaluateInputs()
     {
-        var outputSignal = true;
-        for (var i = 0; i < _inputs.Length; i++)
+        Output.SetSignal(_set.Count == _numberOfInputs);
+    }
+
+    int IPerfectHasher<AndGateGadgetInput>.NumberOfItems => _numberOfInputs;
+    int IPerfectHasher<AndGateGadgetInput>.Hash(AndGateGadgetInput item) => item.Id;
+    AndGateGadgetInput IPerfectHasher<AndGateGadgetInput>.UnHash(int index) => throw new NotSupportedException();
+
+    private sealed class AndGateGadgetInput : IGadgetInput
+    {
+        public readonly int Id;
+        private readonly AndGateGadget _gadget;
+        public string InputName { get; }
+
+        public AndGateGadgetInput(
+            int id,
+            string inputName,
+            AndGateGadget gadget)
         {
-            var input = _inputs[i];
-            outputSignal &= input.Signal;
+            Id = id;
+            InputName = inputName;
+            _gadget = gadget;
         }
-        Output.SetSignal(outputSignal);
+
+        public void OnRegistered()
+        {
+        }
+
+        public void ReactToSignal(bool signal)
+        {
+            if (signal)
+            {
+                _gadget._set.Add(this);
+            }
+            else
+            {
+                _gadget._set.Remove(this);
+            }
+            _gadget.EvaluateInputs();
+        }
     }
 }
--- NeoLemmixSharp.Engine/Level/Gadgets/FunctionalGadgets/BinaryLogic/OrGateGadget.cs
+++ NeoLemmixSharp.Engine/Level/Gadgets/FunctionalGadgets/BinaryLogic/OrGateGadget.cs
@@ -1,10 +1,13 @@
-﻿using NeoLemmixSharp.Engine.Level.Orientations;
+﻿using NeoLemmixSharp.Common.Util.Collections;
+using NeoLemmixSharp.Engine.Level.Gadgets.Interactions;
+using NeoLemmixSharp.Engine.Level.Orientations;
 
 namespace NeoLemmixSharp.Engine.Level.Gadgets.FunctionalGadgets.BinaryLogic;
 
-public sealed class OrGateGadget : LogicGateGadget
+public sealed class OrGateGadget : LogicGateGadget, IPerfectHasher<OrGateGadget.OrGateGadgetInput>
 {
-    private readonly LogicGateGadgetInput[] _inputs;
+    private readonly int _numberOfInputs;
+    private readonly SimpleSet<OrGateGadget, OrGateGadgetInput> _set;
 
     public OrGateGadget(
         int id,
@@ -15,25 +18,56 @@ public sealed class OrGateGadget : LogicGateGadget
         if (inputNames.Length < 2)
             throw new ArgumentException("Expected at least 2 inputs!");
 
-        _inputs = new LogicGateGadgetInput[inputNames.Length];
+        _numberOfInputs = inputNames.Length;
+        _set = new SimpleSet<OrGateGadget, OrGateGadgetInput>(this, false);
 
         for (var i = 0; i < inputNames.Length; i++)
         {
-            var inputName = inputNames[i];
-            var input = new LogicGateGadgetInput(inputName, this);
-            _inputs[i] = input;
+            var input = new OrGateGadgetInput(i, inputNames[i], this);
             RegisterInput(input);
         }
     }
 
     public override void EvaluateInputs()
     {
-        var outputSignal = false;
-        for (var i = 0; i < _inputs.Length; i++)
+        Output.SetSignal(_set.Count > 0);
+    }
+
+    int IPerfectHasher<OrGateGadgetInput>.NumberOfItems => _numberOfInputs;
+    int IPerfectHasher<OrGateGadgetInput>.Hash(OrGateGadgetInput item) => item.Id;
+    OrGateGadgetInput IPerfectHasher<OrGateGadgetInput>.UnHash(int index) => throw new NotSupportedException();
+
+    private sealed class OrGateGadgetInput : IGadgetInput
+    {
+        public readonly int Id;
+        private readonly OrGateGadget _gadget;
+        public string InputName { get; }
+
+        public OrGateGadgetInput(
+            int id,
+            string inputName,
+            OrGateGadget gadget)
         {
-            var input = _inputs[i];
-            outputSignal |= input.Signal;
+            Id = id;
+            InputName = inputName;
+            _gadget = gadget;
         }
-        Output.SetSignal(outputSignal);
+
+        public void OnRegistered()
+        {
+        }
+
+        public void ReactToSignal(bool signal)
+        {
+            if (signal)
+            {
+                _gadget._set.Add(this);
+            }
+            else
+            {
+                _gadget._set.Remove(this);
+            }
+            _gadget.EvaluateInputs();
+        }
     }
 }
