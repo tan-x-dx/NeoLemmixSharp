bdb89f54a98ac51444af0052d72d8df505a8c545
tan-x-dx <andrewhayman19@gmail.com>
2025-01-30 21:21:11 +0000

    More tweaks.

--- NeoLemmixSharp.Engine/Level/Gadgets/HitBoxGadgets/LemmingFiltering/LemmingActionCriterion.cs
+++ NeoLemmixSharp.Engine/Level/Gadgets/HitBoxGadgets/LemmingFiltering/LemmingActionCriterion.cs
@@ -1,16 +1,22 @@
-﻿using NeoLemmixSharp.Engine.Level.LemmingActions;
+﻿using NeoLemmixSharp.Common.Util.Collections.BitArrays;
 using NeoLemmixSharp.Engine.Level.Lemmings;
+using static NeoLemmixSharp.Engine.Level.LemmingActions.LemmingAction;
 
 namespace NeoLemmixSharp.Engine.Level.Gadgets.HitBoxGadgets.LemmingFiltering;
 
 public sealed class LemmingActionCriterion : ILemmingCriterion
 {
-    private readonly LemmingActionSet _allowedActions = LemmingAction.CreateEmptySimpleSet();
+    private LemmingActionBitBuffer _lemmingActionBits;
 
     public void RegisterActions(LemmingActionSet actions)
     {
-        _allowedActions.UnionWith(actions);
+        actions.WriteTo(_lemmingActionBits);
     }
 
-    public bool LemmingMatchesCriteria(Lemming lemming) => _allowedActions.Contains(lemming.CurrentAction);
+    public bool LemmingMatchesCriteria(Lemming lemming)
+    {
+        ReadOnlySpan<uint> bits = _lemmingActionBits;
+        var actionComparer = new LemmingActionComparer();
+        return BitArrayHelpers.GetBit(bits, actionComparer.Hash(lemming.CurrentAction));
+    }
 }
