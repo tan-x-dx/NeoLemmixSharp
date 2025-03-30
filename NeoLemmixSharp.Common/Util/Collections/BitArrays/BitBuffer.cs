620fee352471cc73ed83a430570fd08b477465c9
tan-x-dx <andrewhayman19@gmail.com>
2025-03-30 16:18:53 +0100

    Changes to types.

--- NeoLemmixSharp.Common/Util/Collections/BitArrays/BitBuffer.cs
+++ NeoLemmixSharp.Common/Util/Collections/BitArrays/BitBuffer.cs
@@ -37,66 +37,6 @@ public struct BitBuffer32 : IBitBuffer
     public readonly ReadOnlySpan<uint> AsReadOnlySpan() => MemoryMarshal.CreateReadOnlySpan(in _0, BitBuffer32Length);
 }
 
-[InlineArray(BitBuffer64Length)]
-public struct BitBuffer64 : IBitBuffer
-{
-    private const int BitBuffer64Length = 64 / 32;
-
-    private uint _0;
-
-    public readonly int Length => BitBuffer64Length;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public Span<uint> AsSpan() => MemoryMarshal.CreateSpan(ref _0, BitBuffer64Length);
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly ReadOnlySpan<uint> AsReadOnlySpan() => MemoryMarshal.CreateReadOnlySpan(in _0, BitBuffer64Length);
-}
-
-[InlineArray(BitBuffer96Length)]
-public struct BitBuffer96 : IBitBuffer
-{
-    private const int BitBuffer96Length = 96 / 32;
-
-    private uint _0;
-
-    public readonly int Length => BitBuffer96Length;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public Span<uint> AsSpan() => MemoryMarshal.CreateSpan(ref _0, BitBuffer96Length);
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly ReadOnlySpan<uint> AsReadOnlySpan() => MemoryMarshal.CreateReadOnlySpan(in _0, BitBuffer96Length);
-}
-
-[InlineArray(BitBuffer128Length)]
-public struct BitBuffer128 : IBitBuffer
-{
-    private const int BitBuffer128Length = 128 / 32;
-
-    private uint _0;
-
-    public readonly int Length => BitBuffer128Length;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public Span<uint> AsSpan() => MemoryMarshal.CreateSpan(ref _0, BitBuffer128Length);
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly ReadOnlySpan<uint> AsReadOnlySpan() => MemoryMarshal.CreateReadOnlySpan(in _0, BitBuffer128Length);
-}
-
-[InlineArray(BitBuffer256Length)]
-public struct BitBuffer256 : IBitBuffer
-{
-    private const int BitBuffer256Length = 256 / 32;
-
-    private uint _0;
-
-    public readonly int Length => BitBuffer256Length;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public Span<uint> AsSpan() => MemoryMarshal.CreateSpan(ref _0, BitBuffer256Length);
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public readonly ReadOnlySpan<uint> AsReadOnlySpan() => MemoryMarshal.CreateReadOnlySpan(in _0, BitBuffer256Length);
-}
-
 public readonly struct ArrayBitBuffer : IBitBuffer
 {
     private readonly int _start;
--- NeoLemmixSharp.Common/Util/GameInput/InputController.cs
+++ NeoLemmixSharp.Common/Util/GameInput/InputController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework.Input;
 using NeoLemmixSharp.Common.Util.Collections.BitArrays;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace NeoLemmixSharp.Common.Util.GameInput;
@@ -8,14 +9,14 @@ namespace NeoLemmixSharp.Common.Util.GameInput;
 public sealed class InputController :
     IPerfectHasher<Keys>,
     IPerfectHasher<InputAction>,
-    IBitBufferCreator<BitBuffer256>
+    IBitBufferCreator<InputController.KeysBitBuffer>
 {
     private const int NumberOfKeys = 256;
 
-    private readonly List<KeyToInputMapping> _keyMapping = [];
-    private readonly BitArraySet<InputController, BitBuffer256, Keys> _pressedKeys;
-    private readonly BitArraySet<InputController, BitBuffer256, Keys> _releasedKeys;
-    private readonly List<InputAction> _inputActions = [];
+    private readonly List<KeyToInputMapping> _keyMapping = new(16);
+    private readonly BitArraySet<InputController, KeysBitBuffer, Keys> _pressedKeys;
+    private readonly BitArraySet<InputController, KeysBitBuffer, Keys> _releasedKeys;
+    private readonly List<InputAction> _inputActions = new(16);
 
     private int _previousScrollValue;
 
@@ -33,8 +34,8 @@ public sealed class InputController :
 
     public InputController()
     {
-        _pressedKeys = new BitArraySet<InputController, BitBuffer256, Keys>(this, false);
-        _releasedKeys = new BitArraySet<InputController, BitBuffer256, Keys>(this, false);
+        _pressedKeys = new BitArraySet<InputController, KeysBitBuffer, Keys>(this, false);
+        _releasedKeys = new BitArraySet<InputController, KeysBitBuffer, Keys>(this, false);
 
         LeftMouseButtonAction = CreateInputAction("Left Mouse Button");
         RightMouseButtonAction = CreateInputAction("Right Mouse Button");
@@ -134,7 +135,7 @@ public sealed class InputController :
     int IPerfectHasher<InputAction>.NumberOfItems => _inputActions.Count;
     int IPerfectHasher<InputAction>.Hash(InputAction item) => item.Id;
     InputAction IPerfectHasher<InputAction>.UnHash(int index) => _inputActions[index];
-    void IBitBufferCreator<BitBuffer256>.CreateBitBuffer(out BitBuffer256 buffer) => buffer = new();
+    void IBitBufferCreator<KeysBitBuffer>.CreateBitBuffer(out KeysBitBuffer buffer) => buffer = new();
 
     private readonly struct KeyToInputMapping(Keys key, InputAction inputAction)
     {
@@ -147,5 +148,20 @@ public sealed class InputController :
             key = Key;
             inputAction = InputAction;
         }
+    }
+
+    [InlineArray(KeysBitBufferLength)]
+    private struct KeysBitBuffer : IBitBuffer
+    {
+        private const int KeysBitBufferLength = NumberOfKeys / 32;
+
+        private uint _0;
+
+        public readonly int Length => KeysBitBufferLength;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Span<uint> AsSpan() => MemoryMarshal.CreateSpan(ref _0, KeysBitBufferLength);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public readonly ReadOnlySpan<uint> AsReadOnlySpan() => MemoryMarshal.CreateReadOnlySpan(in _0, KeysBitBufferLength);
     }
 }
