e1cee5db33e876cc276821d6fc4b4949f2ee0cf5
tan-x-dx <andrewhayman19@gmail.com>
2025-04-22 21:36:03 +0100

    Fixed method.

--- NeoLemmixSharp.Common/Util/Collections/BitArrays/BitArrayHelpers.cs
+++ NeoLemmixSharp.Common/Util/Collections/BitArrays/BitArrayHelpers.cs
@@ -198,13 +198,13 @@ public static class BitArrayHelpers
 
         switch (span.Length)
         {
-            case 7: span[6] |= other[6]; goto case 6;
-            case 6: span[5] |= other[5]; goto case 5;
-            case 5: span[4] |= other[4]; goto case 4;
-            case 4: span[3] |= other[3]; goto case 3;
-            case 3: span[2] |= other[2]; goto case 2;
-            case 2: span[1] |= other[1]; goto case 1;
-            case 1: span[0] |= other[0]; return;
+            case 7: span[6] &= other[6]; goto case 6;
+            case 6: span[5] &= other[5]; goto case 5;
+            case 5: span[4] &= other[4]; goto case 4;
+            case 4: span[3] &= other[3]; goto case 3;
+            case 3: span[2] &= other[2]; goto case 2;
+            case 2: span[1] &= other[1]; goto case 1;
+            case 1: span[0] &= other[0]; return;
             case 0: return;
         }
 
