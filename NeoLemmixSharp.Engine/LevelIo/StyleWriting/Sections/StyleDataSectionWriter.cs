fbfdfbdb6acd23c4a1bee7826c2e42a65d404f1f
tan-x-dx <andrewhayman19@gmail.com>
2025-05-05 16:13:10 +0100

    Added new class.

---
+++ NeoLemmixSharp.Engine/LevelIo/StyleWriting/Sections/StyleDataSectionWriter.cs
@@ -0,0 +1,17 @@
+﻿using NeoLemmixSharp.Engine.LevelIo.Data;
+
+namespace NeoLemmixSharp.Engine.LevelIo.StyleWriting.Sections;
+
+public abstract class StyleDataSectionWriter
+{
+    public abstract LevelFileSectionIdentifier SectionIdentifier { get; }
+    public abstract bool IsNecessary { get; }
+
+    public ReadOnlySpan<byte> GetSectionIdentifierBytes() => SectionIdentifier.GetSectionIdentifierBytes();
+
+    public abstract ushort CalculateNumberOfItemsInSection(LevelData levelData);
+
+    public abstract void WriteSection(
+        RawLevelFileDataWriter writer,
+        LevelData levelData);
+}
