ee9728aaa31bbb433a61ceb0d760f253e6226ee9
tan x dx <andrewhayman19@gmail.com>
2025-07-09 22:16:26 +0100

    Changes to methods.

--- NeoLemmixSharp.Engine/Level/LemmingActions/ClimberAction.cs
+++ NeoLemmixSharp.Engine/Level/LemmingActions/ClimberAction.cs
@@ -30,68 +30,18 @@ public sealed class ClimberAction : LemmingAction
         ref var lemmingPosition = ref lemming.AnchorPosition;
         var physicsFrame = lemming.PhysicsFrame;
 
-        bool foundClip;
-
         if (physicsFrame <= 3)
-        {
-            foundClip =
-                PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.Move(lemmingPosition, -dx, 6 + physicsFrame)) ||
-                (PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.Move(lemmingPosition, -dx, 5 + physicsFrame)) &&
-                 !lemming.IsStartingAction);
-
-            if (physicsFrame == 0) // first triggered after 8 frames!
-            {
-                foundClip = foundClip &&
-                            PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.Move(lemmingPosition, -dx, 7));
-            }
-
-            if (foundClip)
-            {
-                // Don't fall below original position on hitting terrain in first cycle
-                if (!lemming.IsStartingAction)
-                {
-                    lemmingPosition = orientation.MoveUp(lemmingPosition, 3 - physicsFrame);
-                }
-
-                if (lemming.State.IsSlider)
-                {
-                    lemmingPosition = orientation.MoveUp(lemmingPosition, 1);
-                    SliderAction.Instance.TransitionLemmingToAction(lemming, false);
-
-                    return true;
-                }
-
-                lemmingPosition = orientation.MoveLeft(lemmingPosition, dx);
-                FallerAction.Instance.TransitionLemmingToAction(lemming, true);
-                lemming.DistanceFallen++; // Least-impact way to fix a fall distance inconsistency. See https://www.lemmingsforums.net/index.php?topic=5794.0
-
-                return true;
-            }
-
-            if (PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.MoveUp(lemmingPosition, 7 + physicsFrame)))
-                return true;
-
-            // if-case prevents too deep bombing, see http://www.lemmingsforums.net/index.php?topic=2620.0
-            if (!(lemming.IsStartingAction && physicsFrame == 1))
-            {
-                lemmingPosition = orientation.MoveUp(lemmingPosition, physicsFrame - 2);
-                lemming.IsStartingAction = false;
-            }
-
-            HoisterAction.Instance.TransitionLemmingToAction(lemming, false);
-
-            return true;
-        }
+            return InitialFrameChecks(lemming, gadgetsNearLemming, dx, orientation, lemmingPosition, physicsFrame);
 
         lemmingPosition = orientation.MoveUp(lemmingPosition, 1);
         lemming.IsStartingAction = false;
 
-        foundClip = PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.Move(lemmingPosition, -dx, 7));
+        var foundClip = PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.Move(lemmingPosition, -dx, 7));
 
-        if (physicsFrame == 7)
+        if (physicsFrame == 7 &&
+            !PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.MoveUp(lemmingPosition, 7)))
         {
-            foundClip = foundClip &&
-                        PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.MoveUp(lemmingPosition, 7));
+            foundClip = false;
         }
 
         if (!foundClip)
@@ -108,6 +58,62 @@ public sealed class ClimberAction : LemmingAction
 
         lemmingPosition = orientation.MoveLeft(lemmingPosition, dx);
         FallerAction.Instance.TransitionLemmingToAction(lemming, true);
+
+        return true;
+    }
+
+    private static bool InitialFrameChecks(
+        Lemming lemming,
+        GadgetEnumerable gadgetsNearLemming,
+        int dx,
+        Orientation orientation,
+        Point lemmingPosition,
+        int physicsFrame)
+    {
+        var foundClip = PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.Move(lemmingPosition, -dx, 6 + physicsFrame)) ||
+                       (PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.Move(lemmingPosition, -dx, 5 + physicsFrame)) &&
+                        !lemming.IsStartingAction);
+
+        if (physicsFrame == 0 && // first triggered after 8 frames!
+            !PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.Move(lemmingPosition, -dx, 7)))
+        {
+            foundClip = false;
+        }
+
+        if (foundClip)
+        {
+            // Don't fall below original position on hitting terrain in first cycle
+            if (!lemming.IsStartingAction)
+            {
+                lemmingPosition = orientation.MoveUp(lemmingPosition, 3 - physicsFrame);
+            }
+
+            if (lemming.State.IsSlider)
+            {
+                lemmingPosition = orientation.MoveUp(lemmingPosition, 1);
+                SliderAction.Instance.TransitionLemmingToAction(lemming, false);
+
+                return true;
+            }
+
+            lemmingPosition = orientation.MoveLeft(lemmingPosition, dx);
+            FallerAction.Instance.TransitionLemmingToAction(lemming, true);
+            lemming.DistanceFallen++; // Least-impact way to fix a fall distance inconsistency. See https://www.lemmingsforums.net/index.php?topic=5794.0
+
+            return true;
+        }
+
+        if (PositionIsSolidToLemming(in gadgetsNearLemming, lemming, orientation.MoveUp(lemmingPosition, 7 + physicsFrame)))
+            return true;
+
+        // if-case prevents too deep bombing, see http://www.lemmingsforums.net/index.php?topic=2620.0
+        if (!(lemming.IsStartingAction && physicsFrame == 1))
+        {
+            lemmingPosition = orientation.MoveUp(lemmingPosition, physicsFrame - 2);
+            lemming.IsStartingAction = false;
+        }
+
+        HoisterAction.Instance.TransitionLemmingToAction(lemming, false);
 
         return true;
     }
