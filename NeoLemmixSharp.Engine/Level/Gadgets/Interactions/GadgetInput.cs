20529023825efa8d46009cf96b8236db581672e5
tan x dx <andrewhayman19@gmail.com>
2025-07-12 15:23:45 +0100

    Fixed methods.

--- NeoLemmixSharp.Engine/Level/Gadgets/Interactions/GadgetInput.cs
+++ NeoLemmixSharp.Engine/Level/Gadgets/Interactions/GadgetInput.cs
@@ -15,8 +15,8 @@ public abstract class GadgetInput : IEquatable<GadgetInput>
     public virtual void OnRegistered() { }
     public abstract void ReactToSignal(bool signal);
 
-    public bool Equals(GadgetInput? other) => other is not null && string.Equals(InputName, other.InputName);
-    public sealed override bool Equals([NotNullWhen(true)] object? obj) => obj is GadgetInput other && string.Equals(InputName, other.InputName);
+    public bool Equals(GadgetInput? other) => other is not null && InputName.Equals(other.InputName);
+    public sealed override bool Equals([NotNullWhen(true)] object? obj) => obj is GadgetInput other && InputName.Equals(other.InputName);
     public sealed override int GetHashCode() => InputName.GetHashCode();
     public sealed override string ToString() => InputName.ToString();
 }
