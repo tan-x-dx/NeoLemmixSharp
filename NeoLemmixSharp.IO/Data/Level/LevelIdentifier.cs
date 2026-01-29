c4d527364321962630ca9ed9fbe1c0e1efafb398
tan x dx <andrewhayman19@gmail.com>
2026-01-29 11:12:09 +0000

    Skip these trivial methods in the debugger.

--- NeoLemmixSharp.IO/Data/Level/LevelIdentifier.cs
+++ NeoLemmixSharp.IO/Data/Level/LevelIdentifier.cs
@@ -1,3 +1,6 @@
-﻿namespace NeoLemmixSharp.IO.Data.Level;
+﻿using System.Diagnostics;
 
+namespace NeoLemmixSharp.IO.Data.Level;
+
+[DebuggerStepThrough]
 public readonly record struct LevelIdentifier(ulong LevelId);
