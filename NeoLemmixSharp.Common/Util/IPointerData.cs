a12693126f41c4ee93a954aa026bc68763ea769b
tan x dx <andrewhayman19@gmail.com>
2026-02-04 14:18:07 +0000

    Moved helper class to be next to its relevant interface.

--- NeoLemmixSharp.Common/Util/IPointerData.cs
+++ NeoLemmixSharp.Common/Util/IPointerData.cs
@@ -1,8 +1,30 @@
-﻿namespace NeoLemmixSharp.Common.Util;
+﻿using System.Runtime.CompilerServices;
+
+namespace NeoLemmixSharp.Common.Util;
 
 public interface IPointerData<T>
     where T : unmanaged, IPointerData<T>
 {
     static abstract T Create(nint dataRef);
     static abstract int SizeInBytes { get; }
+}
+
+public static class PointerDataHelper
+{
+    /// <summary>
+    /// Creates an instance of the desired type, incrementing the <paramref name="dataRef"/> parameter by the correct amount.
+    /// </summary>
+    /// <typeparam name="T">The type to create.</typeparam>
+    /// <param name="dataRef">Represents the pointer the data will utilise. This value is incremented by the amount specified by the type.</param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T CreateItem<T>(ref nint dataRef)
+        where T : unmanaged, IPointerData<T>
+    {
+        var result = T.Create(dataRef);
+
+        dataRef += T.SizeInBytes;
+
+        return result;
+    }
 }
